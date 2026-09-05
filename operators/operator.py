711aa4b4bcade0216a708f8b25baf360f07ce995
JasvantGirase <girasejasvant22@gmail.com>
2026-09-05 16:02:38 +0530

    Create operator.py

---
+++ operators/operator.py
@@ -0,0 +1,11 @@
+
+# a = float(input("Enter first value: "))
+# b = float(input("Enter second value: "))
+
+# print("Addition =", a + b)
+# print("Subtraction =", a - b)
+# print("Multiplication =", a * b)
+# print("Division =", a / b)
+# print("Floor divison=", a //b)
+# print("Power=", a**b)
+

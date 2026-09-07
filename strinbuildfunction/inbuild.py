feec63cec9964745aa4355aa7c1f4bd436d863b3
JasvantGirase <girasejasvant22@gmail.com>
2026-09-07 22:42:50 +0530

    Create inbuild.py

---
+++ strinbuildfunction/inbuild.py
@@ -0,0 +1,82 @@
+#ex1:strip()
+# data="      embedded"
+# print("string is:",data)
+# print("data using string function:",data.strip())
+
+
+#ex2 split()
+# data="my location is pune"
+# print("string is:",data)
+# print("data using split function:",data.split())
+
+
+#ex3 replace()
+# data="my location is pune"
+# print("original str is",data)
+# print("after change")
+# print(data.replace("pune","delhi"))
+
+
+#ex4 count()
+# data="my location is pune"
+# print("count of'i' is:",data.count('i'))
+
+
+#ex5 upper() and lower()
+# Data = "My Location is PUNE Pune"
+
+# print("String --> ", Data)
+# print("String In Upper Case: ", Data.upper())
+# print("String In Lower Case: ", Data.lower())
+
+
+
+#ex6 find()
+# Data = "My Location is PUNE Pune"
+
+# print("String --> ", Data)
+
+# res = 0
+# res = Data.find("i")
+
+# print("Result is --> ", res)
+
+
+# ex7 capitalize() and tite()
+# data="i am trainer"
+# print("data:-",data)
+# print("after change:-",data.capitalize())
+# print("second change:-",data.title())
+
+
+#ex8 swapcase()
+# data="my  Name is JasvANt"
+# print("after swapping the letters",data.swapcase())
+
+
+#ex9 startswith()
+# Data = "My Class Name Is TECHNO SCRIPTS"
+
+# print("Data :- ", Data)
+
+# res = Data.startswith("My")
+
+# print("Result --> ", res)
+
+#ex10 endswith()
+# Data = "My Class Name Is TECHNO SCRIPTS"
+
+# print("Data :- ", Data)
+
+# res = Data.endswith("SCRIPTS")
+
+# print("Result --> ", res)
+
+#ex11
+# Data = "My Class Name Is TECHNO SCRIPTS"
+
+# print("Data :- ", Data)
+
+# ind = Data.index("TECHNO")
+
+# print("Index No. of 'TECHNO' ", ind)

84105a3c02c2859adb38fc793e6aeea8709ca50f
JasvantGirase <girasejasvant22@gmail.com>
2026-09-07 22:42:30 +0530

    Create formatting.py

---
+++ string/formatting.py
@@ -0,0 +1,30 @@
+#ex 1 type 1:normal formatting
+# name="jasvant"
+# middlename="sudamsing"
+# surname="girase"
+# print(name,middlename,surname)
+
+#ex2 type 1:normal formatting
+# C_Name = "Techno scripts"
+# B_No = 116
+# Topic = "Python Lang"
+
+# print("Class Name is", C_Name,
+#       "\nBatch No is", B_No,
+#       "\nCurrent Topic is", Topic)
+
+#ex3 type 2:format function
+# C_Name = "Techno scripts"
+# B_No = 116
+# Topic = "Python Lang"
+
+# print("My Class Name is {} \nMy Batch No is: {}\nCurrent topic is: {}".format(C_Name, B_No, Topic))
+
+
+
+#ex4 format shortcut
+# C_Name = "Techno scripts"
+# B_No = 116
+# Topic = "Python Lang"
+
+# print(f"My Class name is {C_Name} \nMy Batch No is {B_No}\nCurrent Topic is {Topic}")

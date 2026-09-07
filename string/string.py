954140e08d074581fe15af39b979ad8ec4925319
JasvantGirase <girasejasvant22@gmail.com>
2026-09-07 22:41:56 +0530

    Create string.py

---
+++ string/string.py
@@ -0,0 +1,29 @@
+#ex1 Create two string
+# var1="jasvant"
+# var2='girase'
+# print("start name:",var1)
+# print("end name:",var2)
+
+#ex2
+# Name="My name is 'Swapnil'"
+# Str2='I am "teacher"'
+# print(Name)
+# print(Str2)
+
+#ex3 
+# Name="My name is \"Swapnil\"" #escape sewuence we use here(\)
+# Str2="I am \"teacher\""
+# print(Name)
+# print(Str2)
+
+#ex4 Multiline string
+# var1="""
+#       Name=jasvant
+#       ID=08
+#       Address=pune
+#      """
+# print(var1)
+
+
+
+

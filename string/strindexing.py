4923a3013b1959dc563990e9c59578c8ee7267ad
JasvantGirase <girasejasvant22@gmail.com>
2026-09-07 22:42:16 +0530

    Create strindexing.py

---
+++ string/strindexing.py
@@ -0,0 +1,18 @@
+#ex1
+# name="JASVANT"
+# print(name[0])
+# print(name[-7])
+
+
+#ex2
+# Name="DYPATIL"
+# print(Name[:])
+
+
+#ex3
+# Name="DYPATIL"
+# print(Name[0:7:2])
+
+#ex4
+# Name="DYPATIL"
+# print(Name[::-1])

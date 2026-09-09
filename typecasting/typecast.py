ee24e25f68f8dd6c359b99e2d166ce2e2cb18d7e
JasvantGirase <girasejasvant22@gmail.com>
2026-09-09 13:53:55 +0530

    Create typecast.py

---
+++ typecasting/typecast.py
@@ -0,0 +1,32 @@
+#EX1 
+#IMPLICIT TYPE CASTING
+# n1=100
+# n2=12.34
+# print("data type of n1",type(n1))
+# print("data type of n2",type(n2))
+# res=n1+n2
+# print("answer is",res)
+# print("data type of res is",type(res))
+
+
+#EX2
+#EXPLICIT TYPECASTING
+# n1=120
+# print("data type of n1",type(n1))
+# print("data",n1)
+# print("after casting")
+# n1=str(n1)
+# print("data type of n1",type(n1))
+# print("data",n1)
+# print("length",len(n1))
+
+
+#EX3
+#EXPLICIT TYPECASTING
+# val='123.34'
+# print("string is ",val)
+# print("data type ",type(val))
+# print("after casting")
+# val=float(val)
+# print("value is ",val)
+# print("data type ",type(val))

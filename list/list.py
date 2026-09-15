fcc5a15ff1bd4e4f820224dc87a5d079452b5f23
JasvantGirase <girasejasvant22@gmail.com>
2026-09-16 00:37:00 +0530

    Create list.py

---
+++ list/list.py
@@ -0,0 +1,154 @@
+#EX1
+#PRINT THE LIST
+# LST=[100,"TECHNO@",61,True,90.87,'#']
+# print("LIST DATA IS:")
+# print(LST)
+
+
+#EX2
+#TAKE 3 NUMBERS IP FROM USER AND PRINT 
+# n1 = int(input("Enter 1st number: "))
+# n2 = int(input("Enter 2nd number: "))
+# n3 = int(input("Enter 3rd number: "))
+
+# LST = [n1, n2, n3]
+
+# print("List is:", LST)
+
+
+#EX3
+#INDEXING
+# LST=[100,"TECHNO@",61,True,90.87,'#']
+# print("LIST DATA IS:")
+# print(LST[4])
+# print(LST[-4])
+
+#EX4
+#SLICING
+# LST = [10, 20, 30, 40, 50, 60]
+
+# print(LST[1:4])
+# print(LST[1:4:2])
+
+
+#EX 5
+#CONCATATION
+# LST = [100, "TECHNO@", 61, True, 90.87, '#']
+
+# print("LIST1 DATA IS:")
+# print(LST)
+
+# L2 = ["PUNE", 12, 14, 61]
+
+# RES = LST + L2
+
+# print("\nLIST2 DATA IS:")
+# print(L2)
+
+# print("\nRESULT")
+# print(RES)
+
+# print("Data type is:", type(RES))
+
+#ex 6
+#append()
+# LST = [180, "TECHNO@", 61, True, 98.87, '#']
+
+# print("LIST1 DATA IS:")
+# print(LST)
+
+# LST.append(12)
+
+# print("\nList After Change:")
+# print(LST)
+
+
+#ex7
+#pop()
+# LST = [100, "TECHNO@", 61, True, 98.87, '#']
+
+# print("LIST1 DATA IS:")
+# print(LST)
+
+# LST.pop()
+
+# print("\nList After Change:")
+# print(LST)
+
+#ex8
+#remove
+# LST = [100, "TECHNO@", 61, True, 98.87, '#']
+
+# print("LIST1 DATA IS:")
+# print(LST)
+
+# LST.remove(61)
+
+# print("\nList After Change:")
+# print(LST)
+
+#ex 9
+#reverse
+# LST = [100, "TECHNO@", 61, True, 98.87, '#']
+
+# print("LIST1 DATA IS:")
+# print(LST)
+
+# LST.reverse()
+
+# print("\nList After Change:")
+# print(LST)
+
+
+#ex10
+#sorting
+# LST = [100, 61, 98.87, 51]
+
+# print("LIST DATA IS:")
+# print(LST)
+
+# LST.sort()
+
+# print("\nList After sort:")
+# print(LST)
+
+#ex11
+#sorting in reverse way
+# LST = [100, 61, 98.87, 51]
+
+# print("LIST DATA IS:")
+# print(LST)
+
+# LST.sort(reverse=True)
+
+# print("\nList After sort:")
+# print(LST)
+
+
+#ex12
+# LST = [108, "TECHNO@", 61, True, 90.87, '#']
+
+# print("LIST DATA:")
+# print(LST)
+
+# # Operation
+# LST.insert(2, "SCRIPTS_")
+
+# print("\nList After Change:")
+# print(LST)
+
+
+#ex13
+# extend()
+# LST = [100, "TECHNO@", 61, True, 98.87, '#']
+
+# L1 = [99, 22, 42, 37, 58, 11]
+
+# print("LIST DATA:")
+# print(LST)
+
+# # Operation
+# LST.extend([12, "Python_Lang", 6.7])
+
+# print("\nLIST:")
+# print(LST)

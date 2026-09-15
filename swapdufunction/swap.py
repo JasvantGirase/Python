a240afdfe85e9d2104a34bf0a5c53d0dd6c9f027
JasvantGirase <girasejasvant22@gmail.com>
2026-09-16 00:37:16 +0530

    Create swap.py

---
+++ swapdufunction/swap.py
@@ -0,0 +1,32 @@
+# #ex1
+# n1 = int(input("Enter 1st No."))
+# n2 = int(input("Enter 2nd No."))
+
+# print("Before Swapping :")
+# print("Number 1 :- ",n1)
+# print("Number 2 :- ",n2)
+
+# # Operation
+# temp = n1
+# n1 = n2
+# n2 = temp
+
+# print("\nAfter Swapping :")
+# print("Number 1 :- ",n1)
+# print("Number 2 :- ",n2)
+
+
+# #ex2
+#swaping by using packing method
+# n1 = int(input("Enter 1st No."))
+# n2 = int(input("Enter 2nd No."))
+
+# print("Before Swapping :")
+# print("Number 1 :- ",n1)
+# print("Number 2 :- ",n2)
+
+# # Operation
+# n1,n2=n2,n1
+# print("\nAfter Swapping :")
+# print("Number 1 :- ",n1)
+# print("Number 2 :- ",n2)

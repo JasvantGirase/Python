b4a4d7c69580eae2ea44e62da3e30b509594bc6e
JasvantGirase <girasejasvant22@gmail.com>
2026-09-16 00:37:12 +0530

    Update operator.py

--- operators/operator.py
+++ operators/operator.py
@@ -1,11 +1,31 @@
+#ex1
+# # a = float(input("Enter first value: "))
+# # b = float(input("Enter second value: "))
 
-# a = float(input("Enter first value: "))
-# b = float(input("Enter second value: "))
+# # print("Addition =", a + b)
+# # print("Subtraction =", a - b)
+# # print("Multiplication =", a * b)
+# # print("Division =", a / b)
+# # print("Floor divison=", a //b)
+# # print("Power=", a**b)
 
-# print("Addition =", a + b)
-# print("Subtraction =", a - b)
-# print("Multiplication =", a * b)
-# print("Division =", a / b)
-# print("Floor divison=", a //b)
-# print("Power=", a**b)
+# #ex2
+# n1 = int(input("Enter 1st No.: "))
+# n2 = int(input("Enter 2nd No.: "))
+
+# Res = n1 == n2
+
+
+# print("1st Number is bigger than 2nd:", Res)
+
+
+
+#ex3
+
+# n1 = int(input("Enter 1st No.: "))
+# n2 = int(input("Enter 2nd No.: "))
+
+# Res = n1 == n2
+
+# print("Both Values are equal", Res)
 

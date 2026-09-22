7676ad57ce7a0071c7016cf7f3b12807a5b3e3b8
JasvantGirase <girasejasvant22@gmail.com>
2026-09-23 01:48:57 +0530

    Create set.py

---
+++ set/set.py
@@ -0,0 +1,62 @@
+
+#EX1
+#SET
+# DATA={12,16.03,"PYTHON",True}
+# print("set",DATA)
+
+
+#EX2
+# DATA = {12, 16.03, "PYTHON", True}
+
+# print("Set:", DATA)
+
+# DATA.add('@')
+
+# print("Set after adding new data:")
+
+# print(DATA)
+
+
+
+#EX3
+# DATA = {12, 16.03, "PYTHON", True}
+
+# print("Set:", DATA)
+
+# DATA.add('@')
+
+# print("Set after adding new data:")
+
+# print(DATA)
+# DATA=DATA.union({1,2,3,4,5})
+# print("Set after adding new values:")
+
+# print(DATA)
+
+#EX4
+#REMOVE
+# DATA = {12, 16.03, "PYTHON", True}
+
+# print("Set:", DATA)
+# DATA.remove("PYTHON")
+# print("set",DATA)
+
+#EX5
+#POP
+# DATA = {12, 16.03, "PYTHON", True}
+
+# print("Set:", DATA)
+# DATA.pop()
+# print("\nset",DATA)
+
+#EX6
+# Info = set(map(int, input("Enter int Data: ").split()))
+
+# print("Set --> ", Info)
+
+# print("Data Type of Info --> ", type(Info))
+
+
+
+
+

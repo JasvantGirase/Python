8cec2aa3a4752ce9a9e95c0927672b25d539a040
JasvantGirase <girasejasvant22@gmail.com>
2026-09-23 01:46:49 +0530

    Create dict.py

---
+++ dictionary/dict.py
@@ -0,0 +1,68 @@
+
+#EX1
+# CAR={"NAME":"XUV 700","BRAND":"MAHINDRA","PRICE":2500000}
+# print(CAR)
+# print(CAR["NAME"])
+# print(CAR["BRAND"])
+
+
+
+#EX2
+# CAR={"NAME":"XUV 700","BRAND":"MAHINDRA","PRICE":2500000}
+# print(CAR)
+# print(CAR["NAME"])
+# print(CAR["BRAND"])
+# NEW_NAME=input("ENTER NEW CAR NAME:")
+# CAR["NAME"]=NEW_NAME
+# print("DICTIONARY AFTER CHANGE")
+# print(CAR)
+
+#EX3
+#UPDATE
+# CAR = {"Name": "M5_COM", "Brand": "BMW", "Price": 2000000}
+
+# print("Dictionary -- ")
+# print(CAR)
+
+# print("Car Name is: ", CAR["Name"])
+# print("Brand name is: ", CAR["Brand"])
+
+# New_name = input("Enter New Car name: ")
+# CAR["Name"] = New_name
+
+# Brand_Name = input("Enter New Brand Name: ")
+# CAR.update({"Brand": Brand_Name})
+
+# print("Dictionary After Change --> ")
+# print(CAR)
+
+
+
+#EX4
+# CAR = {"Name": "M5_COM", "Brand": "BMW", "Price": 2000000}
+
+# print(CAR)
+# print(CAR.keys())
+# print(CAR.items())
+
+
+#EX5
+#MULTIFUNCTIONAL DICTIONARY
+# CITY={"INDIA":{"MAHARASHTRA":["PUNE","NASHIK","KOLHAPUR"]}}
+# print(CITY)
+# print("CURRENT_LOCATION:",CITY["INDIA"]["MAHARASHTRA"][0])
+
+
+
+#EX6
+#TAKE DICTIONARY AS A INPUT
+# import ast
+# DIC=input("Enter Dictionary in list form")
+# DIC=ast.literal_eval(DIC)
+# print("DICTIONARY")
+# print(DIC)
+
+
+
+
+

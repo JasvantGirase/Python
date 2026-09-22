fb202d202522a3705b9a13a1f798141f6f24c0f8
JasvantGirase <girasejasvant22@gmail.com>
2026-09-23 01:47:44 +0530

    Create filehandling.py

---
+++ filehandling/filehandling.py
@@ -0,0 +1,46 @@
+#FILE HANDLING
+#ex1
+#READ
+# DATA =open("FILE_TXT","x")
+# print("file is open")
+# DATA.close()
+# print("file is closed")
+
+#ex2
+#WRITE
+# DATA =open("FILE_TXT","w")
+# print("file is open")
+# DATA.write("FC ROAD")
+# DATA.close()
+# print("file is closed")
+
+
+#EX3
+# N1 = int(input("Enter first number: "))
+# N2 = int(input("Enter second number: "))
+
+# ANS = N1 + N2
+
+# DATA = open("FILE.txt", "w")
+
+# print("FILE IS OPEN")
+
+# DATA.write("ANSWER = ")
+# DATA.write(str(ANS))
+
+# print("operation done")
+
+# DATA.close()
+
+# print("operation close")
+
+
+#EX4
+# DATA=open("FILE_TXT","r")
+# print("FILE IS OPEN")
+# print("DATA is:")
+# print(DATA.read())
+# print()
+# print("opertaion done")
+# DATA.close()
+# print("File is closed")

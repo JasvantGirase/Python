29acb5dac64bcf718bd311555df72ebebc216768
JasvantGirase <girasejasvant22@gmail.com>
2026-09-23 01:48:36 +0530

    Create oops.py

---
+++ oops/oops.py
@@ -0,0 +1,15 @@
+# # CLASS
+
+# class Values:
+#     a = 100
+#     b = "@PUNE"
+#     c = 20.03
+
+# # Object
+# V = Values()
+
+# print("Int Data is --> ", V.a)
+# print("String is --> ", V.b)
+# print("Float is --> ", V.c)
+
+
